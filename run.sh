2aa6268d70125e792178c63d868ae6d839895758
melferink <m.elferink@umcutrecht.nl>
2018-08-17 10:07:10 +0200

    added output submit file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sh ./scripts/pipeline.2.0.sh
+sh ./scripts/pipeline.2.0.sh | tee submit_log.txt
 #qsub -l h_rt=00:05:00 -l h_vmem=1G -N "mail_negative" -hold_jid "collect3_negative" ./scripts/mail.sh "negative"
