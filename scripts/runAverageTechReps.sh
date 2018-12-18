f9b4ccb0579d8da631b09558677d2a337501f309
Nienke <n.vanunen@umcutrecht.nl>
2018-12-18 12:15:40 +0100

    Update runAverageTechReps.sh

--- scripts/runAverageTechReps.sh
+++ scripts/runAverageTechReps.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 scripts=$1
 outdir=$2
-nrepl=$3
+indir=$3
+nrepl=$4
+thresh2remove=$5
+dimsThresh=$6
 
 echo "### Inputs runAverageTechReps.sh ############################################"
 echo "	scripts:	$scripts"
 echo "	outdir:		$outdir"
+echo "	indir:		$indir"
 echo "  nrepl:    $nrepl"
+echo "	thresh2remove:		$thresh2remove"
+echo "	dimsThresh:		$dimsThresh"
 echo "#############################################################################"
 
-echo "Run file $file in R"
 echo "`pwd`"
 
 module load R
-R --slave --no-save --no-restore --no-environ --args $outdir $nrepl< "$scripts/averageTechReplicates.R"
+R --slave --no-save --no-restore --no-environ --args $outdir $indir $nrepl $thresh2remove $dimsThresh < "$scripts/averageTechReplicates.R"
