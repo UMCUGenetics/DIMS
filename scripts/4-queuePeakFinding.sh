b30af5bcc8682c6b002f95421f27d32e67d96d31
Nienke <n.vanunen@umcutrecht.nl>
2019-01-15 12:00:41 +0100

    improved logging 2.0

--- scripts/4-queuePeakFinding.sh
+++ scripts/4-queuePeakFinding.sh
@@ -16,11 +16,11 @@ adducts=$10
 
 find "$OUTDIR/average_pklist" -iname $label | while read sample;
  do
-     qsub -l h_rt=00:30:00 -l h_vmem=8G -N "peakFinding_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}_${sample}.txt' -e $LOGDIR/'${JOB_NAME}_${sample}.txt' $SCRIPTS/5-runPeakFinding.sh $sample $OUTDIR $scanmode $thresh $resol $SCRIPTS/R
+     qsub -l h_rt=00:30:00 -l h_vmem=8G -N "peakFinding_$scanmode" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME'"_${sample}.txt" -e $LOGDIR/'$JOB_NAME'"_${sample}.txt" $SCRIPTS/5-runPeakFinding.sh $sample $OUTDIR $scanmode $thresh $resol $SCRIPTS/R
      #Rscript peakFinding.2.0.R $sample $SCRIPTS $OUTDIR $thresh $resol $scanmode
  done
 
-qsub -l h_rt=00:15:00 -l h_vmem=8G -N "collect_$scanmode" -hold_jid "peakFinding_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}.txt' -e $LOGDIR/'${JOB_NAME}.txt' $SCRIPTS/6-runCollectSamples.sh $OUTDIR $scanmode $SCRIPTS/R
+qsub -l h_rt=00:15:00 -l h_vmem=8G -N "collect_$scanmode" -hold_jid "peakFinding_$scanmode" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME.txt' -e $LOGDIR/'$JOB_NAME.txt' $SCRIPTS/6-runCollectSamples.sh $OUTDIR $scanmode $SCRIPTS/R
 #Rscript collectSamples.R $OUTDIR $scanmode $SCRIPTS
 
-qsub -l h_rt=00:10:00 -l h_vmem=1G -N "queueGrouping_$scanmode" -hold_jid "collect_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}.txt' -e $LOGDIR/'${JOB_NAME}.txt' $SCRIPTS/7-queuePeakGrouping.sh $INDIR $OUTDIR $SCRIPTS $JOBS $ERRORS $MAIL $scanmode $thresh $label $adducts
+qsub -l h_rt=00:10:00 -l h_vmem=1G -N "queueGrouping_$scanmode" -hold_jid "collect_$scanmode" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME.txt' -e $LOGDIR/'$JOB_NAME.txt' $SCRIPTS/7-queuePeakGrouping.sh $INDIR $OUTDIR $SCRIPTS $JOBS $ERRORS $MAIL $scanmode $thresh $label $adducts
--- scripts/7-queuePeakGrouping.sh
+++ scripts/7-queuePeakGrouping.sh
@@ -16,11 +16,11 @@ adducts=$10
 
 find "$OUTDIR/hmdb_part" -iname "${scanmode}_*" | while read hmdb;
  do
-     qsub -l h_rt=01:00:00 -l h_vmem=8G -N "grouping_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}_${hmdb}.txt' -e $LOGDIR/'${JOB_NAME}_${hmdb}.txt' $SCRIPTS/8-runPeakGrouping.sh $hmdb $OUTDIR $scanmode $resol $SCRIPTS/R
+     qsub -l h_rt=01:00:00 -l h_vmem=8G -N "grouping_$scanmode" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME'"_${hmdb}.txt" -e $LOGDIR/'$JOB_NAME'"_${hmdb}.txt" $SCRIPTS/8-runPeakGrouping.sh $hmdb $OUTDIR $scanmode $resol $SCRIPTS/R
      #Rscript peakGrouping.2.0.R $hmdb $SCRIPTS $OUTDIR $resol $scanmode
  done
 
-qsub -l h_rt=00:15:00 -l h_vmem=8G -N "collect1_$scanmode" -hold_jid "grouping_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}.txt' -e $LOGDIR/'${JOB_NAME}.txt' $SCRIPTS/9-runCollectSamplesGroupedHMDB.sh $OUTDIR $scanmode $SCRIPTS/R
+qsub -l h_rt=00:15:00 -l h_vmem=8G -N "collect1_$scanmode" -hold_jid "grouping_$scanmode" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME.txt' -e $LOGDIR/'$JOB_NAME.txt' $SCRIPTS/9-runCollectSamplesGroupedHMDB.sh $OUTDIR $scanmode $SCRIPTS/R
 #Rscript collectSamplesGroupedHMDB.R $OUTDIR $scanmode
 
-qsub -l h_rt=00:10:00 -l h_vmem=1G -N "queueGroupingRest_$scanmode" -hold_jid "collect1_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}.txt' -e $LOGDIR/'${JOB_NAME}.txt' $SCRIPTS/10-queuePeakGroupingRest.sh $INDIR $OUTDIR $SCRIPTS $JOBS $ERRORS $MAIL $scanmode $thresh $label $adducts
+qsub -l h_rt=00:10:00 -l h_vmem=1G -N "queueGroupingRest_$scanmode" -hold_jid "collect1_$scanmode" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME.txt' -e $LOGDIR/'$JOB_NAME.txt' $SCRIPTS/10-queuePeakGroupingRest.sh $INDIR $OUTDIR $SCRIPTS $JOBS $ERRORS $MAIL $scanmode $thresh $label $adducts
