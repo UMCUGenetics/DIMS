c45e5a5fd6d78c70bdd16f9f7f6320fb61d0465d
Nienke <n.vanunen@umcutrecht.nl>
2019-01-15 11:52:36 +0100

    improved logging

--- scripts/10-queuePeakGroupingRest.sh
+++ scripts/10-queuePeakGroupingRest.sh
@@ -16,8 +16,8 @@ adducts=$10
 
 find "$OUTDIR/specpks_all_rest" -iname "${scanmode}_*" | while read file;
  do
-     qsub -l h_rt=01:00:00 -l h_vmem=8G -N "grouping2_$scanmode" -m as -M $MAIL -o $JOBS -e $ERRORS $SCRIPTS/11-runPeakGroupingRest.sh $file $OUTDIR $scanmode $resol $SCRIPTS/R
+     qsub -l h_rt=01:00:00 -l h_vmem=8G -N "grouping2_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}_${file}.txt' -e $LOGDIR/'${JOB_NAME}_${file}.txt' $SCRIPTS/11-runPeakGroupingRest.sh $file $OUTDIR $scanmode $resol $SCRIPTS/R
      #Rscript peakGrouping.2.0.rest.R $file $SCRIPTS $OUTDIR $resol $scanmode
  done
 
-qsub -l h_rt=01:00:00 -l h_vmem=8G -N "queueFillMissing_$scanmode" -hold_jid "grouping2_$scanmode" -m as -M $MAIL -o $JOBS -e $ERRORS $SCRIPTS/12-queuefillMissing.sh $INDIR $OUTDIR $SCRIPTS $JOBS $ERRORS $MAIL $scanmode $thresh $label $adducts
+qsub -l h_rt=01:00:00 -l h_vmem=8G -N "queueFillMissing_$scanmode" -hold_jid "grouping2_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}.txt' -e $LOGDIR/'${JOB_NAME}.txt' $SCRIPTS/12-queuefillMissing.sh $INDIR $OUTDIR $SCRIPTS $JOBS $ERRORS $MAIL $scanmode $thresh $label $adducts
--- scripts/15-queueSumAdducts.sh
+++ scripts/15-queueSumAdducts.sh
@@ -3,7 +3,7 @@
 $INDIR=$1
 $OUTDIR=$2
 $SCRIPTS=$3
-$JOBS=$4
+$LOGDIR=$4
 $ERRORS=$5
 $MAIL=$6
 
@@ -16,11 +16,11 @@ adducts=$10
 
 find "$OUTDIR/hmdb_part_adductSums" -iname "${scanmode}_*" | while read hmdb;
  do
-  qsub -l h_rt=02:00:00 -l h_vmem=8G -N "sumAdducts_$scanmode" -m as -M $MAIL -o $JOBS -e $ERRORS $SCRIPTS/16-runSumAdducts.sh $hmdb $OUTDIR $scanmode $adducts $SCRIPTS/R
+  qsub -l h_rt=02:00:00 -l h_vmem=8G -N "sumAdducts_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}_${hmdb}.txt' -e $LOGDIR/'${JOB_NAME}_${hmdb}.txt' $SCRIPTS/16-runSumAdducts.sh $hmdb $OUTDIR $scanmode $adducts $SCRIPTS/R
   #Rscript runSumAdducts.R $hmdb $scanmode $OUTDIR $adducts $SCRIPTS
 done
 
-qsub -l h_rt=00:30:00 -l h_vmem=8G -N "collect3_$scanmode" -hold_jid "sumAdducts_$scanmode" -m as -M $MAIL -o $JOBS -e $ERRORS $SCRIPTS/17-runCollectSamplesAdded.sh $OUTDIR $scanmode $SCRIPTS/R
+qsub -l h_rt=00:30:00 -l h_vmem=8G -N "collect3_$scanmode" -hold_jid "sumAdducts_$scanmode" -m as -M $MAIL -o $LOGDIR/'${JOB_NAME}.txt' -e $LOGDIR/'${JOB_NAME}.txt' $SCRIPTS/17-runCollectSamplesAdded.sh $OUTDIR $scanmode $SCRIPTS/R
 #Rscript collectSamplesAdded.R $OUTDIR $scanmode
 
 #qsub -l h_rt=00:05:00 -l h_vmem=500M -N "mail_$scanmode" -hold_jid "sumAdducts_$scanmode" -m as -M $MAIL -o $JOBS -e $ERRORS $SCRIPTS/18-mail.sh $MAIL
