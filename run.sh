cf6a31e3670920749eac9ad7d834f3dbb86a74ce
Nienke <n.vanunen@umcutrecht.nl>
2019-01-16 12:07:07 +0100

    test having queue script make the run scripts itself

--- run.sh
+++ run.sh
@@ -125,12 +125,14 @@ find $INDIR -iname "*.mzXML" | sort | while read mzXML;
      it=$((it+1))
 
      if [ $it == 1 ] && [ ! -f $OUTDIR/breaks.fwhm.RData ] ; then # || [[ $it == 2 ]]
-       qsub -l h_rt=00:05:00 -l h_vmem=1G -N "breaks" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME.txt' -e $LOGDIR/'$JOB_NAME.txt' $SCRIPTS/1-runGenerateBreaks.sh $mzXML $OUTDIR $trim $resol $nrepl $SCRIPTS/R
-       #Rscript generateBreaksFwhm.HPC.R $mzXML $OUTDIR $INDIR $trim $resol $nrepl
+       echo "Rscript $SCRIPTS/R/generateBreaksFwhm.HPC.R $mzXML $OUTDIR $trim $resol $nrepl $SCRIPTS/R" > $OUTDIR/jobs/breaks.sh
+       qsub -l h_rt=00:05:00 -l h_vmem=1G -N breaks -m as -M $MAIL -o $OUTDIR/logs/ -e -o $OUTDIR/logs/ $OUTDIR/jobs/breaks.sh
      fi
-     qsub -l h_rt=00:10:00 -l h_vmem=4G -N "dims" -hold_jid "breaks" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME'"_${mzXML##*/}.txt" -e $LOGDIR/'$JOB_NAME'"_${mzXML##*/}.txt" $SCRIPTS/2-runDIMS.sh $mzXML $OUTDIR $trim $dimsThresh $resol $SCRIPTS/R
-     #Rscript DIMS.R $mzXML $OUTDIR $trim $dimsThresh $resol $SCRIPTS
+     echo "Rscript $SCRIPTS/R/DIMS.R $mzXML $OUTDIR $trim $dimsThresh $resol $SCRIPTS/R" > $OUTDIR/jobs/dims_${it}.sh
+     qsub -l h_rt=00:10:00 -l h_vmem=4G -N dims -hold_jid breaks -m as -M $MAIL -o $OUTDIR/logs/ -e -o $OUTDIR/logs/ $OUTDIR/jobs/dims_${it}.sh
  done
+
+exit 0
 
 qsub -l h_rt=01:30:00 -l h_vmem=5G -N "average" -hold_jid "dims" -m as -M $MAIL -o $LOGDIR/'$JOB_NAME.txt' -e $LOGDIR/'$JOB_NAME.txt' $SCRIPTS/3-runAverageTechReps.sh $INDIR $OUTDIR $nrepl $thresh2remove $dimsThresh $SCRIPTS/R
 #Rscript averageTechReplicates.R $OUTDIR $INDIR $nrepl $thresh2remove $dimsThresh
