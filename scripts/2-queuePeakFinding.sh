7b5c886d0396147582f274966105625dcb077a54
Nienke <n.vanunen@umcutrecht.nl>
2019-01-23 09:36:02 +0100

    Again

--- scripts/2-queuePeakFinding.sh
+++ scripts/2-queuePeakFinding.sh
@@ -20,7 +20,7 @@ find "$OUTDIR/average_pklist" -iname $label | sort | while read sample;
    qsub -l h_rt=00:30:00 -l h_vmem=8G -N "peakFinding_${scanmode}_${input}" -m as -M $MAIL -o $OUTDIR/logs/4-peakFinding.2.0 -e $OUTDIR/logs/4-peakFinding.2.0 $OUTDIR/jobs/4-peakFinding.2.0/${scanmode}_${input}.sh
  done
 
-echo "Rscript $SCRIPTS/R/5-collectSamples.R $OUTDIR $scanmode $SCRIPTS/R" > $OUTDIR/jobs/5-collectSamples_${scanmode}.sh
-qsub -l h_rt=00:15:00 -l h_vmem=8G -N "collect_$scanmode" -hold_jid "peakFinding_${scanmode}_*" -m as -M $MAIL -o $OUTDIR/logs/5-collectSamples -e $OUTDIR/logs/5-collectSamples $OUTDIR/jobs/5-collectSamples_${scanmode}.sh
+echo "Rscript $SCRIPTS/R/5-collectSamples.R $OUTDIR $scanmode $SCRIPTS/R" > $OUTDIR/jobs/5-collectSamples/${scanmode}.sh
+qsub -l h_rt=00:15:00 -l h_vmem=8G -N "collect_$scanmode" -hold_jid "peakFinding_${scanmode}_*" -m as -M $MAIL -o $OUTDIR/logs/5-collectSamples -e $OUTDIR/logs/5-collectSamples $OUTDIR/jobs/5-collectSamples/${scanmode}.sh
 
 qsub -l h_rt=00:10:00 -l h_vmem=1G -N "queueGrouping_$scanmode" -hold_jid "collect_$scanmode" -m as -M $MAIL -o $OUTDIR/logs/queue/3-queuePeakGrouping -e $OUTDIR/logs/queue/3-queuePeakGrouping $SCRIPTS/3-queuePeakGrouping.sh $INDIR $OUTDIR $SCRIPTS $LOGDIR $MAIL $scanmode $thresh $label $adducts
--- scripts/6-queueSumAdducts.sh
+++ scripts/6-queueSumAdducts.sh
@@ -20,7 +20,7 @@ find "$OUTDIR/hmdb_part_adductSums" -iname "${scanmode}_*" | sort | while read h
     qsub -l h_rt=02:00:00 -l h_vmem=8G -N "sumAdducts_${scanmode}_${input}" -m as -M $MAIL -o $OUTDIR/logs/11-runSumAdducts -e $OUTDIR/logs/11-runSumAdducts $OUTDIR/jobs/11-runSumAdducts/${scanmode}_${input}.sh
 done
 
-echo "Rscript $SCRIPTS/R/12-collectSamplesAdded.R $OUTDIR $scanmode $SCRIPTS/R" > $OUTDIR/jobs/12-collectSamplesAdded_${scanmode}.sh
-qsub -l h_rt=00:30:00 -l h_vmem=8G -N "collect3_$scanmode" -hold_jid "sumAdducts_${scanmode}_*" -m ase -M $MAIL -o $OUTDIR/logs/12-collectSamplesAdded -e $OUTDIR/logs/12-collectSamplesAdded $OUTDIR/jobs/12-collectSamplesAdded_${scanmode}.sh
+echo "Rscript $SCRIPTS/R/12-collectSamplesAdded.R $OUTDIR $scanmode $SCRIPTS/R" > $OUTDIR/jobs/12-collectSamplesAdded/${scanmode}.sh
+qsub -l h_rt=00:30:00 -l h_vmem=8G -N "collect3_$scanmode" -hold_jid "sumAdducts_${scanmode}_*" -m ase -M $MAIL -o $OUTDIR/logs/12-collectSamplesAdded -e $OUTDIR/logs/12-collectSamplesAdded $OUTDIR/jobs/12-collectSamplesAdded/${scanmode}.sh
 
 #qsub -l h_rt=00:05:00 -l h_vmem=500M -N "mail_$scanmode" -hold_jid "sumAdducts_$scanmode" -m as -M $MAIL -o $JOBS -e $ERRORS $SCRIPTS/18-mail.sh $MAIL
