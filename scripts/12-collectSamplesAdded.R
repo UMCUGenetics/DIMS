59784136c79dc59732a3df5ad8da0bf2aff3d6d2
Nienke <n.vanunen@umcutrecht.nl>
2019-07-01 13:07:57 +0200

    fix creation done file

--- scripts/12-collectSamplesAdded.R
+++ scripts/12-collectSamplesAdded.R
@@ -5,16 +5,18 @@
 run <- function(resultDir, scanmode) {
   # resultDir="./results"
   # scanmode="negative"
-  
+
   object.files = list.files(paste(resultDir, "adductSums", sep="/"), full.names=TRUE, pattern=paste(scanmode, "_", sep=""))
-  
+
   outlist.tot=NULL
   for (i in 1:length(object.files)) {
     load(object.files[i])
     outlist.tot = rbind(outlist.tot, adductsum)
   }
-  
-  save(outlist.tot, file=paste(resultDir, "/adductSums_", scanmode, ".RData", sep=""))
+
+  save(outlist.tot, file=paste0(resultDir, "/adductSums_", scanmode, ".RData"))
+
+  file.create(paste(resultDir, "logs", "done", sep = "/"))
 }
 
 cat("Start collectSamplesAdded.R")
@@ -26,5 +28,4 @@ for (arg in cmd_args) cat("  ", arg, "\n", sep="")
 
 run(cmd_args[1], cmd_args[2])
 
-file.create(paste0(resultDir, "logs", "done", sep = "/"))
 cat("Ready collectSamplesAdded.R")
