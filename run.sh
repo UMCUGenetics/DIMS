280e2a0d33a120ada9dc6b02ac7d2b97506926e2
Nienke <poofcakes@hotmail.com>
2020-02-10 16:12:47 +0100

    first changes from sge to slurm

--- run.sh
+++ run.sh
@@ -137,6 +137,10 @@ mv -f $indir/settings.config_tmp $indir/settings.config
 . $indir/settings.config
 #thresh2remove=$(printf "%.0f" $thresh2remove) # to convert to decimal from scientific notation
 
+# Clear the environment from any previously loaded modules
+#module purge > /dev/null 2>&1
+
+
 module load R/3.2.2
 
 # 1-queueStart.sh
@@ -150,18 +154,20 @@ find $outdir/data -iname "*.mzXML" | sort | while read mzXML;
      input=\$(basename \$mzXML .mzXML)
      if [ \$it == 1 ] && [ ! -f $outdir/breaks.fwhm.RData ] ; then
        echo "Rscript $scripts/1-generateBreaksFwhm.HPC.R \$mzXML $outdir $trim $resol $nrepl $scripts" > $outdir/jobs/1-generateBreaksFwhm.HPC/breaks.sh
-       qsub -q all.q -P dbg_mz -l h_rt=00:05:00 -l h_vmem=1G -N "breaks" -m as -M $email -o $outdir/logs/1-generateBreaksFwhm.HPC -e $outdir/logs/1-generateBreaksFwhm.HPC $outdir/jobs/1-generateBreaksFwhm.HPC/breaks.sh
+       srun --account==dbg_mz --time=5 --mem=1000 --job-name="breaks" --mail-type=END --mail-user=$email --output=$outdir/logs/1-generateBreaksFwhm.HPC --error=$outdir/logs/1-generateBreaksFwhm.HPC $outdir/jobs/1-generateBreaksFwhm.HPC/breaks.sh)
      fi
 
+     exit 1
      echo "Rscript $scripts/2-DIMS.R \$mzXML $outdir $trim $dims_thresh $resol $scripts" > $outdir/jobs/2-DIMS/\${input}.sh
-     qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=4G -N "dims_\${input}" -hold_jid "breaks" -m as -M $email -o $outdir/logs/2-DIMS -e $outdir/logs/2-DIMS $outdir/jobs/2-DIMS/\${input}.sh
+     srun --parsable --account==dbg_mz --time=10 --mem=4000 --job-name="dims_\${input}" -hold_jid "breaks" --mail-type=END --mail-user=$email --output=$outdir/logs/2-DIMS --error=$outdir/logs/2-DIMS $outdir/jobs/2-DIMS/\${input}.sh
  done
 
+exit 2
 echo "Rscript $scripts/3-averageTechReplicates.R $indir $outdir $nrepl $thresh2remove $dims_thresh $scripts" > $outdir/jobs/3-averageTechReplicates/average.sh
-qsub -q all.q -P dbg_mz -l h_rt=01:30:00 -l h_vmem=5G -N "average" -hold_jid "dims_*" -m as -M $email -o $outdir/logs/3-averageTechReplicates -e $outdir/logs/3-averageTechReplicates $outdir/jobs/3-averageTechReplicates/average.sh
+srun --parsable --account==dbg_mz --time=90 --mem=5000 --job-name="average" -hold_jid "dims_*" --mail-type=END --mail-user=$email --output=$outdir/logs/3-averageTechReplicates --error=$outdir/logs/3-averageTechReplicates $outdir/jobs/3-averageTechReplicates/average.sh
 
-qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=500M -N "queueFinding_positive" -hold_jid "average" -m as -M $email -o $outdir/logs/queue/2-queuePeakFinding -e $outdir/logs/queue/2-queuePeakFinding $outdir/jobs/queue/2-queuePeakFinding_positive.sh
-qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=500M -N "queueFinding_negative" -hold_jid "average" -m as -M $email -o $outdir/logs/queue/2-queuePeakFinding -e $outdir/logs/queue/2-queuePeakFinding $outdir/jobs/queue/2-queuePeakFinding_negative.sh
+sbatch --parsable --account==dbg_mz --time=10 --mem=500 --job-name="queueFinding_positive" -hold_jid "average" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/2-queuePeakFinding --error=$outdir/logs/queue/2-queuePeakFinding $outdir/jobs/queue/2-queuePeakFinding_positive.sh
+sbatch --parsable --account==dbg_mz --time=10 --mem=500 --job-name="queueFinding_negative" -hold_jid "average" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/2-queuePeakFinding --error=$outdir/logs/queue/2-queuePeakFinding $outdir/jobs/queue/2-queuePeakFinding_negative.sh
 EOF
 
 # 01-queueConversionCheck.sh
@@ -177,7 +183,7 @@ if [ "\$1" -lt 1 ]; then
     if [ ! -f $outdir/data/\${file}.mzXML ]; then
       echo "\${file} doesn't exist"
       continue=false
-      qsub -q all.q -P dbg_mz -l h_rt=00:03:00 -l h_vmem=4G -N "conversion_\${file}" -m asb -M $email -o $outdir/logs/0-conversion -e $outdir/logs/0-conversion $outdir/jobs/0-conversion/\${file}.sh
+      sbatch --parsable --account==dbg_mz --time=3 --mem=4000 --job-name="conversion_\${file}" --mail-type=ENDb --mail-user=$email --output=$outdir/logs/0-conversion --error=$outdir/logs/0-conversion $outdir/jobs/0-conversion/\${file}.sh
   	fi
   done
 
@@ -189,7 +195,7 @@ if [ "\$1" -lt 1 ]; then
       echo "error \${file}"
       continue=false
       find $outdir/logs/0-conversion -type f -name "*\${file}*" -delete # otherwise there'll be an endless loop
-      qsub -q all.q -P dbg_mz -l h_rt=00:03:00 -l h_vmem=4G -N "conversion_\${file}" -m asb -M $email -o $outdir/logs/0-conversion -e $outdir/logs/0-conversion $outdir/jobs/0-conversion/\${file}.sh
+      sbatch --parsable --account==dbg_mz --time=3 --mem=4000 --job-name="conversion_\${file}" --mail-type=ENDb --mail-user=$email --output=$outdir/logs/0-conversion --error=$outdir/logs/0-conversion $outdir/jobs/0-conversion/\${file}.sh
   	fi
   done
 
@@ -200,7 +206,7 @@ if [ "\$1" -lt 1 ]; then
       echo "output empty \${file}"
       continue=false
       find $outdir/logs/0-conversion -type f -name "*\${file}*" -delete # otherwise there'll be an endless loop
-      qsub -q all.q -P dbg_mz -l h_rt=00:03:00 -l h_vmem=4G -N "conversion_\${file}" -m asb -M $email -o $outdir/logs/0-conversion -e $outdir/logs/0-conversion $outdir/jobs/0-conversion/\${file}.sh
+      \$(sbatch --parsable --account==dbg_mz --time=3 --mem=4000 --job-name="conversion_\${file}" --mail-type=ENDb --mail-user=$email --output=$outdir/logs/0-conversion --error=$outdir/logs/0-conversion $outdir/jobs/0-conversion/\${file}.sh)
     fi
   done
 else
@@ -209,10 +215,10 @@ fi
 
 if [ "\$continue" = true ]; then
   echo continue
-  qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=1G -N "queueStart" -hold_jid "conversion_*" -m as -M $email -o $outdir/logs/queue/1-queueStart -e $outdir/logs/queue/1-queueStart $outdir/jobs/queue/1-queueStart.sh
+  sbatch --parsable --account==dbg_mz --time=10 --mem=1000 --job-name="queueStart" -hold_jid "conversion_*" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/1-queueStart --error=$outdir/logs/queue/1-queueStart $outdir/jobs/queue/1-queueStart.sh
 else
   echo redo conversion check
-  qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=1G -N "queueConversionCheck" -hold_jid "conversion_*" -m as -M $email -o $outdir/logs/queue/ -e $outdir/logs/queue/ $outdir/jobs/queue/01-queueConversionCheck.sh 1
+  sbatch --parsable --account==dbg_mz --time=10 --mem=1000 --job-name="queueConversionCheck" -hold_jid "conversion_*" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/ --error=$outdir/logs/queue/ $outdir/jobs/queue/01-queueConversionCheck.sh 1
 fi
 
 EOF
@@ -221,11 +227,11 @@ EOF
 find $indir -iname "*.raw" | sort | while read raw;
   do
     input=$(basename $raw .raw)
-    echo "singularity exec -B ~/wineprefix64:/wineprefix64,/hpc/dbg_mz $proteowizard.sif wine msconvert $raw -o $outdir/data --mzXML" > $outdir/jobs/0-conversion/${input}.sh
-    qsub -q all.q -P dbg_mz -l h_rt=00:03:00 -l h_vmem=4G -N "conversion_${input}" -m s -M $email -o $outdir/logs/0-conversion -e $outdir/logs/0-conversion $outdir/jobs/0-conversion/${input}.sh
+    echo "singularity exec -B ~/wineprefix64:/wineprefix64,/hpc/dbg_mz $proteowizard.sif wine msconvert $raw --output=$outdir/data --mzXML" > $outdir/jobs/0-conversion/${input}.sh
+    sbatch --parsable --account==dbg_mz --time=3 --mem=4000 --job-name="conversion_${input}" --mail-type=s --mail-user=$email --output=$outdir/logs/0-conversion --error=$outdir/logs/0-conversion $outdir/jobs/0-conversion/${input}.sh
   done
 
-qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=1G -N "queueConversionCheck" -hold_jid "conversion_*" -m as -M $email -o $outdir/logs/queue/01-queueConversionCheck -e $outdir/logs/queue/01-queueConversionCheck $outdir/jobs/queue/01-queueConversionCheck.sh 0
+sbatch --parsable --account==dbg_mz --time=10 --mem=1000 --job-name="queueConversionCheck" -hold_jid "conversion_*" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/01-queueConversionCheck --error=$outdir/logs/queue/01-queueConversionCheck $outdir/jobs/queue/01-queueConversionCheck.sh 0
 
 
 
@@ -245,15 +251,16 @@ find "$outdir/average_pklist" -iname $label | sort | while read sample;
  do
    input=\$(basename \$sample .RData)
    echo "Rscript $scripts/4-peakFinding.R \$sample $outdir $scanmode $thresh $resol $scripts" > $outdir/jobs/4-peakFinding/${scanmode}_\${input}.sh
-   qsub -q all.q -P dbg_mz -l h_rt=01:00:00 -l h_vmem=8G -N "peakFinding_${scanmode}_\${input}" -m as -M $email -o $outdir/logs/4-peakFinding -e $outdir/logs/4-peakFinding $outdir/jobs/4-peakFinding/${scanmode}_\${input}.sh
+   sbatch --parsable --account==dbg_mz --time=60 --mem=8000 --job-name="peakFinding_${scanmode}_\${input}" --mail-type=END --mail-user=$email --output=$outdir/logs/4-peakFinding --error=$outdir/logs/4-peakFinding $outdir/jobs/4-peakFinding/${scanmode}_\${input}.sh
  done
 
 echo "Rscript $scripts/5-collectSamples.R $outdir $scanmode $db" > $outdir/jobs/5-collectSamples/${scanmode}.sh
-qsub -q all.q -P dbg_mz -l h_rt=02:00:00 -l h_vmem=8G -N "collect_$scanmode" -hold_jid "peakFinding_${scanmode}_*" -m as -M $email -o $outdir/logs/5-collectSamples -e $outdir/logs/5-collectSamples $outdir/jobs/5-collectSamples/${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=120 --mem=8000 --job-name="collect_$scanmode" -hold_jid "peakFinding_${scanmode}_*" --mail-type=END --mail-user=$email --output=$outdir/logs/5-collectSamples --error=$outdir/logs/5-collectSamples $outdir/jobs/5-collectSamples/${scanmode}.sh
 
-qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=1G -N "queueGrouping_$scanmode" -hold_jid "collect_$scanmode" -m as -M $email -o $outdir/logs/queue/3-queuePeakGrouping -e $outdir/logs/queue/3-queuePeakGrouping $outdir/jobs/queue/3-queuePeakGrouping_${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=10 --mem=1000 --job-name="queueGrouping_$scanmode" -hold_jid "collect_$scanmode" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/3-queuePeakGrouping --error=$outdir/logs/queue/3-queuePeakGrouping $outdir/jobs/queue/3-queuePeakGrouping_${scanmode}.sh
 EOF
 
+exit 0
 
   # 3-queuePeakGrouping.sh
 cat << EOF >> $outdir/jobs/queue/3-queuePeakGrouping_${scanmode}.sh
@@ -263,13 +270,13 @@ find "$outdir/hmdb_part" -iname "${scanmode}_*" | sort | while read hmdb;
  do
    input=\$(basename \$hmdb .RData)
    echo "Rscript $scripts/6-peakGrouping.R \$hmdb $outdir $scanmode $resol $scripts" > $outdir/jobs/6-peakGrouping/${scanmode}_\${input}.sh
-   qsub -q all.q -P dbg_mz -l h_rt=02:00:00 -l h_vmem=8G -N "grouping_${scanmode}_\${input}" -m as -M $email -o $outdir/logs/6-peakGrouping -e $outdir/logs/6-peakGrouping $outdir/jobs/6-peakGrouping/${scanmode}_\${input}.sh
+   sbatch --parsable --account==dbg_mz --time=120 --mem=8000 --job-name="grouping_${scanmode}_\${input}" --mail-type=END --mail-user=$email --output=$outdir/logs/6-peakGrouping --error=$outdir/logs/6-peakGrouping $outdir/jobs/6-peakGrouping/${scanmode}_\${input}.sh
  done
 
 echo "Rscript $scripts/7-collectSamplesGroupedHMDB.R $outdir $scanmode $scripts" > $outdir/jobs/7-collectSamplesGroupedHMDB/${scanmode}.sh
-qsub -q all.q -P dbg_mz -l h_rt=01:00:00 -l h_vmem=8G -N "collect1_$scanmode" -hold_jid "grouping_${scanmode}_*" -m as -M $email -o $outdir/logs/7-collectSamplesGroupedHMDB -e $outdir/logs/7-collectSamplesGroupedHMDB $outdir/jobs/7-collectSamplesGroupedHMDB/${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=60 --mem=8000 --job-name="collect1_$scanmode" -hold_jid "grouping_${scanmode}_*" --mail-type=END --mail-user=$email --output=$outdir/logs/7-collectSamplesGroupedHMDB --error=$outdir/logs/7-collectSamplesGroupedHMDB $outdir/jobs/7-collectSamplesGroupedHMDB/${scanmode}.sh
 
-qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=1G -N "queueGroupingRest_$scanmode" -hold_jid "collect1_$scanmode" -m as -M $email -o $outdir/logs/queue/4-queuePeakGroupingRest -e $outdir/logs/queue/4-queuePeakGroupingRest $outdir/jobs/queue/4-queuePeakGroupingRest_${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=10 --mem=1000 --job-name="queueGroupingRest_$scanmode" -hold_jid "collect1_$scanmode" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/4-queuePeakGroupingRest --error=$outdir/logs/queue/4-queuePeakGroupingRest $outdir/jobs/queue/4-queuePeakGroupingRest_${scanmode}.sh
 EOF
 
   # 4-queuePeakGroupingRest.sh
@@ -280,10 +287,10 @@ find "$outdir/specpks_all_rest" -iname "${scanmode}_*" | sort | while read file;
  do
    input=\$(basename \$file .RData)
    echo "Rscript $scripts/8-peakGrouping.rest.R \$file $outdir $scanmode $resol $scripts" > $outdir/jobs/8-peakGrouping.rest/${scanmode}_\${input}.sh
-   qsub -q all.q -P dbg_mz -l h_rt=01:00:00 -l h_vmem=8G -N "grouping2_${scanmode}_\${input}" -m as -M $email -o $outdir/logs/8-peakGrouping.rest -e $outdir/logs/8-peakGrouping.rest $outdir/jobs/8-peakGrouping.rest/${scanmode}_\${input}.sh
+   sbatch --parsable --account==dbg_mz --time=60 --mem=8000 --job-name="grouping2_${scanmode}_\${input}" --mail-type=END --mail-user=$email --output=$outdir/logs/8-peakGrouping.rest --error=$outdir/logs/8-peakGrouping.rest $outdir/jobs/8-peakGrouping.rest/${scanmode}_\${input}.sh
  done
 
-qsub -q all.q -P dbg_mz -l h_rt=00:20:00 -l h_vmem=8G -N "queueFillMissing_$scanmode" -hold_jid "grouping2_${scanmode}_*" -m as -M $email -o $outdir/logs/queue/5-queueFillMissing -e $outdir/logs/queue/5-queueFillMissing $outdir/jobs/queue/5-queueFillMissing_${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=20 --mem=8000 --job-name="queueFillMissing_$scanmode" -hold_jid "grouping2_${scanmode}_*" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/5-queueFillMissing --error=$outdir/logs/queue/5-queueFillMissing $outdir/jobs/queue/5-queueFillMissing_${scanmode}.sh
 EOF
 
   # 5-queueFillMissing.sh
@@ -294,20 +301,20 @@ find "$outdir/grouping_rest" -iname "${scanmode}_*" | sort | while read rdata;
  do
   input=\$(basename \$rdata .RData)
   echo "Rscript $scripts/9-runFillMissing.R \$rdata $outdir $scanmode $thresh $resol $scripts" > $outdir/jobs/9-runFillMissing/${scanmode}_\${input}.sh
-  qsub -q all.q -P dbg_mz -l h_rt=02:00:00 -l h_vmem=8G -N "peakFilling_${scanmode}_\${input}" -m as -M $email -o $outdir/logs/9-runFillMissing -e $outdir/logs/9-runFillMissing $outdir/jobs/9-runFillMissing/${scanmode}_\${input}.sh
+  sbatch --parsable --account==dbg_mz --time=120 --mem=8000 --job-name="peakFilling_${scanmode}_\${input}" --mail-type=END --mail-user=$email --output=$outdir/logs/9-runFillMissing --error=$outdir/logs/9-runFillMissing $outdir/jobs/9-runFillMissing/${scanmode}_\${input}.sh
  done
 
 find "$outdir/grouping_hmdb" -iname "*_${scanmode}.RData" | sort | while read rdata2;
  do
   input=\$(basename \$rdata2 .RData)
   echo "Rscript $scripts/9-runFillMissing.R \$rdata2 $outdir $scanmode $thresh $resol $scripts" > $outdir/jobs/9-runFillMissing/${scanmode}_\${input}.sh
-  qsub -q all.q -P dbg_mz -l h_rt=02:00:00 -l h_vmem=8G -N "peakFilling2_${scanmode}_\${input}" -hold_jid "peakFilling_${scanmode}_*" -m as -M $email -o $outdir/logs/9-runFillMissing -e $outdir/logs/9-runFillMissing $outdir/jobs/9-runFillMissing/${scanmode}_\${input}.sh
+  sbatch --parsable --account==dbg_mz --time=120 --mem=8000 --job-name="peakFilling2_${scanmode}_\${input}" -hold_jid "peakFilling_${scanmode}_*" --mail-type=END --mail-user=$email --output=$outdir/logs/9-runFillMissing --error=$outdir/logs/9-runFillMissing $outdir/jobs/9-runFillMissing/${scanmode}_\${input}.sh
  done
 
 echo "Rscript $scripts/10-collectSamplesFilled.R $outdir $scanmode $normalization $scripts $db $z_score" > $outdir/jobs/10-collectSamplesFilled/${scanmode}.sh
-qsub -q all.q -P dbg_mz -l h_rt=01:00:00 -l h_vmem=8G -N "collect2_$scanmode" -hold_jid "peakFilling2_${scanmode}_*" -m as -M $email -o $outdir/logs/10-collectSamplesFilled -e $outdir/logs/10-collectSamplesFilled $outdir/jobs/10-collectSamplesFilled/${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=60 --mem=8000 --job-name="collect2_$scanmode" -hold_jid "peakFilling2_${scanmode}_*" --mail-type=END --mail-user=$email --output=$outdir/logs/10-collectSamplesFilled --error=$outdir/logs/10-collectSamplesFilled $outdir/jobs/10-collectSamplesFilled/${scanmode}.sh
 
-qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=1G -N "queueSumAdducts_$scanmode" -hold_jid "collect2_$scanmode" -m as -M $email -o $outdir/logs/queue/6-queueSumAdducts -e $outdir/logs/queue/6-queueSumAdducts $outdir/jobs/queue/6-queueSumAdducts_${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=10 --mem=1000 --job-name="queueSumAdducts_$scanmode" -hold_jid "collect2_$scanmode" --mail-type=END --mail-user=$email --output=$outdir/logs/queue/6-queueSumAdducts --error=$outdir/logs/queue/6-queueSumAdducts $outdir/jobs/queue/6-queueSumAdducts_${scanmode}.sh
 EOF
 
 # 14-cleanup.sh
@@ -325,17 +332,17 @@ find "$outdir/hmdb_part_adductSums" -iname "${scanmode}_*" | sort | while read h
  do
     input=\$(basename \$hmdb .RData)
     echo "Rscript $scripts/11-runSumAdducts.R \$hmdb $outdir $scanmode $adducts $scripts $z_score" > $outdir/jobs/11-runSumAdducts/${scanmode}_\${input}.sh
-    qsub -q all.q -P dbg_mz -l h_rt=03:00:00 -l h_vmem=8G -N "sumAdducts_${scanmode}_\${input}" -m as -M $email -o $outdir/logs/11-runSumAdducts -e $outdir/logs/11-runSumAdducts $outdir/jobs/11-runSumAdducts/${scanmode}_\${input}.sh
+    sbatch --parsable --account==dbg_mz --time=180 --mem=8000 --job-name="sumAdducts_${scanmode}_\${input}" --mail-type=END --mail-user=$email --output=$outdir/logs/11-runSumAdducts --error=$outdir/logs/11-runSumAdducts $outdir/jobs/11-runSumAdducts/${scanmode}_\${input}.sh
 done
 
 echo "Rscript $scripts/12-collectSamplesAdded.R $outdir $scanmode $scripts" > $outdir/jobs/12-collectSamplesAdded/${scanmode}.sh
-qsub -q all.q -P dbg_mz -l h_rt=00:30:00 -l h_vmem=8G -N "collect3_$scanmode" -hold_jid "sumAdducts_${scanmode}_*" -m as -M $email -o $outdir/logs/12-collectSamplesAdded -e $outdir/logs/12-collectSamplesAdded $outdir/jobs/12-collectSamplesAdded/${scanmode}.sh
+sbatch --parsable --account==dbg_mz --time=30 --mem=8000 --job-name="collect3_$scanmode" -hold_jid "sumAdducts_${scanmode}_*" --mail-type=END --mail-user=$email --output=$outdir/logs/12-collectSamplesAdded --error=$outdir/logs/12-collectSamplesAdded $outdir/jobs/12-collectSamplesAdded/${scanmode}.sh
 
 if [ -f "$outdir/logs/done" ]; then   # if one of the scanmodes is already queued
   echo other scanmode already queued - queue next step
   echo "module load R/3.5.1 && Rscript $scripts/13-excelExport.R $outdir $name $matrix $db2 $scripts $z_score" > $outdir/jobs/13-excelExport.sh
-  qsub -q all.q -P dbg_mz -l h_rt=01:00:00 -l h_vmem=8G -N "excelExport" -hold_jid "collect3_*" -m ase -M $email -o $outdir/logs/13-excelExport -e $outdir/logs/13-excelExport $outdir/jobs/13-excelExport.sh
-  qsub -q all.q -P dbg_mz -l h_rt=00:10:00 -l h_vmem=1G -N "cleanup" -hold_jid "excelExport" -m as -M $email -o $outdir/logs/14-cleanup -e $outdir/logs/14-cleanup $outdir/jobs/14-cleanup.sh
+  sbatch --parsable --account==dbg_mz --time=60 --mem=8000 --job-name="excelExport" -hold_jid "collect3_*" --mail-type=ENDe --mail-user=$email --output=$outdir/logs/13-excelExport --error=$outdir/logs/13-excelExport $outdir/jobs/13-excelExport.sh
+  sbatch --parsable --account==dbg_mz --time=10 --mem=1000 --job-name="cleanup" -hold_jid "excelExport" --mail-type=END --mail-user=$email --output=$outdir/logs/14-cleanup --error=$outdir/logs/14-cleanup $outdir/jobs/14-cleanup.sh
 else
   echo other scanmode not queued yet
   touch $outdir/logs/done
