3f4fd5a10a85cadac42f92e51a438bca422e5af6
Nienke <poofcakes@hotmail.com>
2020-02-25 11:36:45 +0100

    allow nrepl arg for makeInit.R

--- scripts/AddOnFunctions/makeInit.R
+++ scripts/AddOnFunctions/makeInit.R
@@ -1,27 +1,30 @@
 #!/usr/bin/env Rscript
 
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
 df <- read.csv(args[1], sep="\t")
-nrepl <- 3
+nrepl <- as.numeric(args[2])
 
-sampleNames=trimws(as.vector(unlist(df$File_Name)))
-nsampgrps = length(sampleNames)/nrepl
-groupNames=trimws(as.vector(unlist(df$Sample_Name)))
-groupNames=gsub('[^-.[:alnum:]]','_',groupNames)
-groupNamesUnique=unique(groupNames)
-groupNamesNotUnique=groupNames[duplicated(groupNames)]
+sampleNames <- trimws(as.vector(unlist(df$File_Name)))
+nsampgrps <- length(sampleNames)/nrepl
+groupNames <- trimws(as.vector(unlist(df$Sample_Name)))
+groupNames <- gsub('[^-.[:alnum:]]','_',groupNames)
+groupNamesUnique <- unique(groupNames)
+groupNamesNotUnique <- groupNames[duplicated(groupNames)]
 
-repl.pattern = c()
+repl.pattern <- c()
 for (a in 1:nsampgrps) {
-  tmp = c()
+  tmp <- c()
   for (b in nrepl:1) {
-    i = ((a*nrepl)-b)+1
+    i <- ((a*nrepl)-b)+1
     tmp <- c(tmp, sampleNames[i])
   }
   repl.pattern <- c(repl.pattern, list(tmp))
 }
 
-names(repl.pattern) = groupNamesUnique
+names(repl.pattern) <- groupNamesUnique
 
-save(repl.pattern, file=args[2],version=2)
+#just to preview
+head(repl.pattern)
+
+save(repl.pattern, file=args[2], version=2)
