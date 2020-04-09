2867c17f94ca9ec8a7339366afe44c5da602dc8b
Nienke <poofcakes@hotmail.com>
2020-04-10 01:01:43 +0200

    Update 4-peakFinding.R

--- pipeline/scripts/4-peakFinding.R
+++ pipeline/scripts/4-peakFinding.R
@@ -26,12 +26,6 @@ sourceDir(paste(scripts, "AddOnFunctions", sep="/"))
 load(paste(outdir, "breaks.fwhm.RData", sep="/"))
 load(filepath)
 
-sampname <- outlist.persample[[1,1]]
-
-#  install.packages("Cairo", lib ="/hpc/local/CentOS6/dbg_mz/R-3.1.2/library")
-#  library( "yourLibrary", lib.loc = "/hpc/local/osversion/group/path" )
-#  library("Cairo")
-
 options(digits=16)
 int.factor=1*10^5 # Number of x used to calc area under Gaussian (is not analytic)
 scale=2 # Initial value used to estimate scaling parameter
@@ -39,16 +33,11 @@ width=1024
 height=768
 
 ### fit Gaussian estimate mean and integrate to obtain intensity
-findPeaks.Gauss.HPC <- function(plist, breaks.fwhm, int.factor, scale, resol, outdir, sampname, scanmode, plot, thresh, width, height) {
-  # plist=pklist$neg
-  # breaks.fwhm=pklist$breaksFwhm
-  # label="/specpks/Neg_specpks"
-  # scanmode="negative"
-  # plot=TRUE
+findPeaks.Gauss.HPC <- function(plist, breaks.fwhm, int.factor, scale, resol, outdir, scanmode, plot, thresh, width, height) {
+  sampname <- plist[[1,1]]
   
   range = as.vector(plist)
   names(range) = rownames(plist)
-  #range[34:43]
   
   values = list("mean"=NULL,"area"=NULL,"nr"=NULL,"min"=NULL,"max"=NULL,"qual"=NULL,"spikes"=0)
   
@@ -56,7 +45,7 @@ findPeaks.Gauss.HPC <- function(plist, breaks.fwhm, int.factor, scale, resol, ou
   
   outlist.persample=NULL
   outlist.persample=cbind("samplenr"=values$nr, "mzmed.pkt"=values$mean, "fq"=values$qual, "mzmin.pkt"=values$min, "mzmax.pkt"=values$max, "height.pkt"=values$area)
-  #outlist.persample=cbind("samplenr"=sample.nr, "mzmed.pkt"=peak.mean, "fq"=peak.qual, "mzmin.pkt"=peak.min, "mzmax.pkt"=peak.max, "height.pkt"=peak.area)
+
   index=which(outlist.persample[,"height.pkt"]==0)
   if (length(index)>0) {
     outlist.persample=outlist.persample[-index,]
@@ -65,14 +54,11 @@ findPeaks.Gauss.HPC <- function(plist, breaks.fwhm, int.factor, scale, resol, ou
   save(outlist.persample, file=paste(outdir, "4-specpks", paste(sampname, "_", scanmode, ".RData", sep=""), sep="/"))
   
   cat(paste("There were", values$spikes, "spikes!"))
-  #return(peaklist.all)
 }
 
 
 if (scanmode == "negative") {
-  # pklist$neg
-  # pklist$breaksFwhm
-  findPeaks.Gauss.HPC(sum_neg, breaks.fwhm, int.factor, scale, resol, outdir, sampname, scanmode, FALSE, thresh, width, height)
+  findPeaks.Gauss.HPC(sum_neg, breaks.fwhm, int.factor, scale, resol, outdir, scanmode, FALSE, thresh, width, height)
 } else {
-  findPeaks.Gauss.HPC(sum_pos, breaks.fwhm, int.factor, scale, resol, outdir, sampname, scanmode, FALSE, thresh, width, height)
+  findPeaks.Gauss.HPC(sum_pos, breaks.fwhm, int.factor, scale, resol, outdir, scanmode, FALSE, thresh, width, height)
 }
