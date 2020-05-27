1552793ab2ffe3bfbe0182e5f5f191c1e34fff1c
Nienke <poofcakes@hotmail.com>
2020-05-27 17:42:49 +0200

    Increase max memory of 11-runSumAdducts job from 4G to 8G

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -398,7 +398,7 @@ for hmdb in ${outdir}/hmdb_part_adductSums/${scanmode}_* ; do
 
   /hpc/local/CentOS7/common/lang/R/3.2.2/bin/Rscript ${scripts}/11-runSumAdducts.R \$hmdb ${outdir} ${scanmode} ${adducts} ${z_score}
   " > ${outdir}/jobs/11-runSumAdducts/${scanmode}_\${input}.sh
-  cur_id=\$(sbatch --job-name=11-runSumAdducts_\${input}_${scanmode}_${name} --time=01:00:00 --mem=4G --output=${outdir}/logs/11-runSumAdducts/${scanmode}_\${input}.o --error=${outdir}/logs/11-runSumAdducts/${scanmode}_\${input}.e ${global_sbatch_parameters} ${outdir}/jobs/11-runSumAdducts/${scanmode}_\${input}.sh)
+  cur_id=\$(sbatch --job-name=11-runSumAdducts_\${input}_${scanmode}_${name} --time=01:00:00 --mem=8G --output=${outdir}/logs/11-runSumAdducts/${scanmode}_\${input}.o --error=${outdir}/logs/11-runSumAdducts/${scanmode}_\${input}.e ${global_sbatch_parameters} ${outdir}/jobs/11-runSumAdducts/${scanmode}_\${input}.sh)
   job_ids+="\${cur_id}:"
 done
 job_ids=\${job_ids::-1}
