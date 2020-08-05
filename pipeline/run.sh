11efddcb5a3e826587aa1f64e15a21734e19dd8e
FiniDG-HAN <31845711+FiniDG-HAN@users.noreply.github.com>
2020-08-05 14:46:57 +0200

    added positive check + e-mail update with missing positive controls

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -226,9 +226,18 @@ for x in \$(find ${outdir}/logs -name "*.e" -not -empty | sort); do
 done
 msg+="</p>"
 msg+="<hr>"
-msg+="Possible that have no results:<br>"
+msg+="Possible samples that have no results:<br>"
 msg+="<p>"
 for x in \$(find ${outdir} -name "sample_names_nodata.txt" -not -empty); do
+  msg+="<b>\${x}</b><br>"
+  msg+=\$(cat \${x})
+  msg+="<br><br>"
+done
+msg+="</p>"
+msg+="<hr>"
+msg+="Missing positive controls:<br>"
+msg+="<p>"
+for x in \$(find ${outdir} -name "missing_positive_controls.txt" -not -empty); do
   msg+="<b>\${x}</b><br>"
   msg+=\$(cat \${x})
   msg+="<br><br>"
