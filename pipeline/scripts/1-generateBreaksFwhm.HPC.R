e85004e83850e8d569950028f006850345469cbb
Ilja van Hoek <ivanhoek@hpcs03.op.umcutrecht.nl>
2022-05-12 12:31:40 +0200

    add stitching, will do for 1 file, as mz range and trimming is determined in this script

--- pipeline/scripts/1-generateBreaksFwhm.HPC.R
+++ pipeline/scripts/1-generateBreaksFwhm.HPC.R
@@ -2,6 +2,8 @@
 
 #.libPaths(new = "/hpc/local/CentOS7/dbg_mz/R_libs/3.2.2")
 suppressPackageStartupMessages(library(mzR))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(stringr))
 # load required packages 
 #suppressPackageStartupMessages(library(xcms))
 
@@ -22,11 +24,90 @@ breaks.fwhm.avg=NULL
 bins=NULL
 posRes=NULL
 negRes=NULL
+TrimlowerMZrange<-function(hdr){
+  for(i in 1:length(hdr$scanWindowLowerLimit)){
+    #if (70%in%hdr$scanWindowLowerLimit[[i]]){print('Lowerlimit of current scan is 70')}
+    if (hdr$scanWindowLowerLimit[[i]]==min(hdr$scanWindowLowerLimit)) {cat('Lowerlimit has reached:', hdr$scanWindowLowerLimit[[i]])}
+    else{hdr$scanWindowLowerLimit[[i]]<-hdr$scanWindowLowerLimit[[i]]+5}
+    
+  } 
+  return(hdr)
+}    
 
-#x <- suppressMessages(xcmsRaw(filepath))
-Dat<-openMSfile(filepath)
-hdr=header(Dat)
-pks=peaks(Dat)
+
+#Trims the MZ range in header file (upper scan window limit)
+TrimupperMZrange<-function(hdr){
+  for(i in 1:length(hdr$scanWindowUpperLimit)){
+    #if (1280%in%hdr$scanWindowUpperLimit[[i]]){print('Upperlimit of current scan is 1280')}
+    if (hdr$scanWindowUpperLimit[[i]]==max(hdr$scanWindowUpperLimit)) {cat('Upperlimit has reached:', hdr$scanWindowUpperLimit[[i]])}  
+    else{hdr$scanWindowUpperLimit[[i]]<-hdr$scanWindowUpperLimit[[i]]-5}
+  } 
+  return(hdr)
+}   
+
+#replace FTMS + p filterstring to correct mz-range
+Replace_FTMS<-function(hdr){
+  for(i in 1:length(hdr$filterString)){
+    hdr$filterString[i]<-str_replace(hdr$filterString[i],"(?<=\\[)\\d+", toString(hdr$scanWindowLowerLimit[i]))
+  } 
+  return(hdr)
+}   
+
+# replace FTMS + upper mz
+Replace_FTMShigh<-function(hdr){
+  for(i in 1:length(hdr$filterString)){
+    hdr$filterString[i]<-str_replace(hdr$filterString[i],"(?<=\\-)\\d+", toString(hdr$scanWindowUpperLimit[i]))
+  } 
+  return(hdr)
+} 
+
+#Removes all peaks outside of the scanwindow lower limit
+TrimPeaklistlower<-function(pks, hdr){
+  for (i in 1:length(pks)) { 
+    #a<-hdr$scanWindowLowerLimit[i]
+    #print(a)
+    x<-which(pks[[i]][,1]<hdr$scanWindowLowerLimit[i],arr.ind = TRUE)
+    #print(x)
+    if(length(x)!=0) {pks[[i]]<-pks[[i]][-x,]}
+  }
+  return(pks)
+  #return(hdr)
+}
+
+#Removes all peaks outside of the scanwindow upper limit
+TrimPeaklistupper<-function(pks, hdr){
+  for (i in 1:length(pks)) {
+    #b<-hdr$scanWindowUpperLimit[i]
+    #print(b)
+    y<-which(pks[[i]][,1]<hdr$scanWindowUppperLimit[i],arr.ind = TRUE)
+    #print(y)
+    if(length(y)!=0) {pks[[i]]<-pks[[i]][-y,]}
+  } 
+  return(pks)
+  #return(hdr)
+}
+
+#Replaces the peakcount, lowMZ and HighMZ in the header file based on the new peaklist
+Replace_low_high_MZ_and_pkCount<-function(hdr, pks){ 
+  for (i in 1:length(pks)) {
+    hdr$peaksCount[[i]] <-length(pks[[i]][,1]) 
+    hdr$lowMZ[[i]]<-min(pks[[i]][,1])
+    hdr$highMZ[[i]]<-max(pks[[i]][,1])
+  }
+  return(hdr)
+}
+Dat <- openMSfile(filepath)
+
+hdr <- header(Dat)
+pks <- spectra(Dat)
+hdr <- TrimlowerMZrange(hdr)
+hdr <- TrimupperMZrange(hdr)
+hdr <- Replace_FTMS(hdr)
+hdr <- Replace_FTMShigh(hdr)
+pks <- TrimPeaklistlower(pks, hdr)
+pks <- TrimPeaklistupper(pks, hdr)
+hdr <- Replace_low_high_MZ_and_pkCount(hdr, pks)
+
 #x <- readMSData(filepath, mode = "onDisk", msLevel. = 1)
 #trimLeft = round(x@featureData@data[["retentionTime"]][length(x@featureData@data[["retentionTime"]])*trim])
 #trimRight = round(x@featureData@data[["retentionTime"]][length(x@featureData@data[["retentionTime"]])*(1-trim)])
@@ -78,5 +159,7 @@ for (i in 1:nsegment) {
   deltaMZ = range[2]-range[1]
   breaks.fwhm.avg <- c(breaks.fwhm.avg, range + 0.5 * deltaMZ)
 }
+
+#writeMSData(object = pks, file = filepath, hdr)
 
 save(breaks.fwhm,breaks.fwhm.avg,trimLeft,trimRight,file=paste(outdir, "breaks.fwhm.RData", sep="/"))
