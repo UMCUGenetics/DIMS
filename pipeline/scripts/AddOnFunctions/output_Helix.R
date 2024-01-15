1bdb79e4a77f22a9a8a23d0648866b62727a8e42
Anne Luesink <anneluesink@live.nl>
2024-01-15 13:40:10 +0100

    Changed gsub to include other digit options

--- pipeline/scripts/AddOnFunctions/output_Helix.R
+++ pipeline/scripts/AddOnFunctions/output_Helix.R
@@ -33,7 +33,7 @@ output_for_Helix <- function(protocol_name, metab_all, metab_interest_sorted){
   
   # Split patient code into labnummer and Onderzoeksnummer
   for (row in 1:nrow(df_metabs_Helix)) {
-    df_metabs_Helix[row,"labnummer"] <- gsub("[P\\.1]", "", df_metabs_Helix[row,"Patient"])
+    df_metabs_Helix[row,"labnummer"] <- gsub("^P|\\.[0-9]*", "", df_metabs_Helix[row,"Patient"])
     labnummer_split <- strsplit(as.character(df_metabs_Helix[row, "labnummer"]), "M")[[1]]
     df_metabs_Helix[row, "Onderzoeksnummer"] <- paste0("MB",labnummer_split[1],"/",labnummer_split[2])
   }
